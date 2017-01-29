12b9caf448755f460602a91b2b2d314d47673475
pawel-gorka <git@pawelgorka.com>
2017-01-29 18:39:14 +0100

    changes

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -1,29 +1,8 @@
-// var IndexPage = require('./index-page');
-
-// describe("hello-protractor", function () {
-
-//   var page = new IndexPage();
-
-//   beforeEach(function() {
-//       page.get();
-//   });
-
-//   describe("index", function () {
-//     it("should display the correct title", function () {
-//       expect(page.getTitle()).toBe('Protractor and Sauce Labs Dummy Page');
-//     });
-//   });
-// });
-
-// spec.js
-describe('Protractor Demo App1', function () {
-  it('should greet the named user', function () {
-    browser.get('http://www.angularjs.org');
-
-    element(by.model('yourName')).sendKeys('Example');
-
-    var greeting = element(by.binding('yourName'));
-
-    expect(greeting.getText()).toEqual('Hello Example!');
+describe("hello-protractor", function () {
+  describe("index", function () {
+    it("should display the correct title", function () {
+      browser.get('/#');
+      expect(browser.getTitle()).toBe('Protractor and Sauce Labs Dummy Page');
+    });
   });
 });
