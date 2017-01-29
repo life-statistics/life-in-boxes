f2df853bbd997fa93593cad210dc7632a5208581
pawel-gorka <git@pawelgorka.com>
2017-01-29 17:37:20 +0100

    simplest sauce labs config

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -1,16 +1,29 @@
-var IndexPage = require('./index-page');
+// var IndexPage = require('./index-page');
 
-describe("hello-protractor", function () {
+// describe("hello-protractor", function () {
 
-  var page = new IndexPage();
+//   var page = new IndexPage();
 
-  beforeEach(function() {
-      page.get();
-  });
+//   beforeEach(function() {
+//       page.get();
+//   });
 
-  describe("index", function () {
-    it("should display the correct title", function () {
-      expect(page.getTitle()).toBe('Protractor and Sauce Labs Dummy Page');
-    });
+//   describe("index", function () {
+//     it("should display the correct title", function () {
+//       expect(page.getTitle()).toBe('Protractor and Sauce Labs Dummy Page');
+//     });
+//   });
+// });
+
+// spec.js
+describe('Protractor Demo App1', function () {
+  it('should greet the named user', function () {
+    browser.get('http://www.angularjs.org');
+
+    element(by.model('yourName')).sendKeys('Example');
+
+    var greeting = element(by.binding('yourName'));
+
+    expect(greeting.getText()).toEqual('Hello Example!');
   });
 });
